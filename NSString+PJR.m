02b3de37aa17687a87f4a239731b4b20e481c99a
Fairy-happy <fairy@fairy.local>
2016-04-13 15:52:16 +0800

    判断各种号码是否合法

---
+++ NSString+PJR.m
@@ -0,0 +1,328 @@
+//
+//  NSString+PJR.m
+//  Lib
+//
+//  Created by Paritosh on 15/05/14.
+//
+// * All rights reserved.
+/*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the <organization> nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+
+#import "NSString+PJR.h"
+
+@implementation NSString (PJR)
+
+
+// Checking if String is Empty
+-(BOOL)isBlank
+{
+    return ([[self removeWhiteSpacesFromString] isEqualToString:@""]) ? YES : NO;
+}
+//Checking if String is empty or nil
+-(BOOL)isValid
+{
+    return ([[self removeWhiteSpacesFromString] isEqualToString:@""] || self == nil || [self isEqualToString:@"(null)"]) ? NO :YES;
+}
+
+// remove white spaces from String
+- (NSString *)removeWhiteSpacesFromString
+{
+	NSString *trimmedString = [self stringByTrimmingCharactersInSet:
+							   [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	return trimmedString;
+}
+
+// Counts number of Words in String
+- (NSUInteger)countNumberOfWords
+{
+    NSScanner *scanner = [NSScanner scannerWithString:self];
+    NSCharacterSet *whiteSpace = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+	
+    NSUInteger count = 0;
+    while ([scanner scanUpToCharactersFromSet: whiteSpace  intoString: nil]) {
+        count++;
+    }
+	
+    return count;
+}
+
+// If string contains substring
+- (BOOL)containsString:(NSString *)subString
+{
+    return ([self rangeOfString:subString].location == NSNotFound) ? NO : YES;
+}
+
+// If my string starts with given string
+- (BOOL)isBeginsWith:(NSString *)string
+{
+    return ([self hasPrefix:string]) ? YES : NO;
+}
+
+// If my string ends with given string
+- (BOOL)isEndssWith:(NSString *)string
+{
+    return ([self hasSuffix:string]) ? YES : NO;
+}
+
+
+
+// Replace particular characters in my string with new character
+- (NSString *)replaceCharacter:(NSString *)olderChar withCharcter:(NSString *)newerChar
+{
+    return  [self stringByReplacingOccurrencesOfString:olderChar withString:newerChar];
+}
+
+// Get Substring from particular location to given lenght
+- (NSString*)getSubstringFrom:(NSInteger)begin to:(NSInteger)end
+{
+	NSRange r;
+	r.location = begin;
+	r.length = end - begin;
+	return [self substringWithRange:r];
+}
+
+// Add substring to main String 
+- (NSString *)addString:(NSString *)string
+{
+    if(!string || string.length == 0)
+        return self;
+
+    return [self stringByAppendingString:string];
+}
+
+// Remove particular sub string from main string
+-(NSString *)removeSubString:(NSString *)subString
+{
+    if ([self containsString:subString])
+    {
+        NSRange range = [self rangeOfString:subString];
+        return  [self stringByReplacingCharactersInRange:range withString:@""];
+    }
+    return self;
+}
+
+
+// If my string contains ony letters
+- (BOOL)containsOnlyLetters
+{
+    NSCharacterSet *letterCharacterset = [[NSCharacterSet letterCharacterSet] invertedSet];
+    return ([self rangeOfCharacterFromSet:letterCharacterset].location == NSNotFound);
+}
+
+// If my string contains only numbers
+- (BOOL)containsOnlyNumbers
+{
+    NSCharacterSet *numbersCharacterSet = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789"] invertedSet];
+    return ([self rangeOfCharacterFromSet:numbersCharacterSet].location == NSNotFound);
+}
+
+// If my string contains letters and numbers
+- (BOOL)containsOnlyNumbersAndLetters
+{
+    NSCharacterSet *numAndLetterCharSet = [[NSCharacterSet alphanumericCharacterSet] invertedSet];
+    return ([self rangeOfCharacterFromSet:numAndLetterCharSet].location == NSNotFound);
+}
+
+// If my string is available in particular array
+- (BOOL)isInThisarray:(NSArray*)array
+{
+    for(NSString *string in array) {
+        if([self isEqualToString:string]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+// Get String from array
++ (NSString *)getStringFromArray:(NSArray *)array
+{
+    return [array componentsJoinedByString:@" "];
+}
+
+// Convert Array from my String
+- (NSArray *)getArray
+{
+    return [self componentsSeparatedByString:@" "];
+}
+
+// Get My Application Version number
++ (NSString *)getMyApplicationVersion
+{
+	NSDictionary *info = [[NSBundle mainBundle] infoDictionary];
+	NSString *version = [info objectForKey:@"CFBundleVersion"];
+	return version;
+}
+
+// Get My Application name
++ (NSString *)getMyApplicationName
+{
+	NSDictionary *info = [[NSBundle mainBundle] infoDictionary];
+	NSString *name = [info objectForKey:@"CFBundleDisplayName"];
+	return name;
+}
+
+
+// Convert string to NSData
+- (NSData *)convertToData
+{
+    return [self dataUsingEncoding:NSUTF8StringEncoding];
+}
+
+// Get String from NSData
++ (NSString *)getStringFromData:(NSData *)data
+{
+    return [[NSString alloc] initWithData:data
+                                 encoding:NSUTF8StringEncoding];
+    
+}
+
+// Is Valid Email
+
+- (BOOL)isValidEmail
+{
+    NSString *regex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTestPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [emailTestPredicate evaluateWithObject:self];
+}
+
+// Is Valid Phone
+
+- (BOOL)isValidAmericanPhoneNumber
+{
+    NSString *regex = @"[235689][0-9]{6}([0-9]{3})?";
+    NSPredicate *test = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [test evaluateWithObject:self];
+}
+
+// Is Valid URL
+
+- (BOOL)isValidUrl
+{
+    NSString *regex =@"(http|https)://((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+";
+    NSPredicate *urlTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    return [urlTest evaluateWithObject:self];
+}
+
+-(BOOL)isValidChinesePhoneNumber{
+    
+    
+    //1[0-9]{10}
+    //^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$
+    //    NSString *regex = @"[0-9]{11}";
+    NSString *regex = @"^1[3|4|5|7|8][0-9]\\d{8}$";
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    BOOL isMatch = [pred evaluateWithObject:self];
+    if (!isMatch) {
+       
+        return NO;
+    }
+    
+    return YES;
+//    NSString * MOBILE = @"^1([37][0-9]|5[0-35-9]|8[025-9]|70|77)\\d{8}$";
+//    
+//    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
+//    
+//    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
+//    
+//    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
+//    
+//    // NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
+//    
+//    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+//    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+//    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+//    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
+//    BOOL res1 = [regextestmobile evaluateWithObject:self];
+//    BOOL res2 = [regextestcm evaluateWithObject:self];
+//    BOOL res3 = [regextestcu evaluateWithObject:self];
+//    BOOL res4 = [regextestct evaluateWithObject:self];
+//    
+//    if (res1 || res2 || res3 || res4 )
+//    {
+//        return YES;
+//    }
+//    else
+//    {
+//        return NO;
+//    }
+    
+}
+
+- (NSString *) reverseString
+{
+    NSUInteger len = [self length];
+    // self 表示字符串本身
+    NSMutableString *retStr = [NSMutableString stringWithCapacity:len];
+    while (len > 0) {
+        unichar c = [self characterAtIndex:--len];
+        // 从后取一个字符 unicode
+        NSLog(@" c is %C", c);
+        NSString *s = [NSString stringWithFormat:
+                       @"%C", c];
+        [retStr appendString:s];
+    }
+    
+    if (retStr != nil && retStr.length > 0) {
+        return [retStr copy];
+    }else
+    {
+        return nil;
+    }
+}
+
+#pragma mark -
+#pragma mark Resize Height
+
+//-(CGFloat)frameHeighWithFontSize:(CGFloat)fontSize viewWidth:(CGFloat)width
+//{
+//    NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:fontSize]};
+//    CGSize size = CGSizeMake(0, 0);
+////    if (iOS7) {
+////        size  = [self sizeWithAttributes:attributes];
+////        
+////    }else
+////    {
+////        size = CGSizeMake(fontSize*self.length, fontSize*1.3);
+////    }
+//    
+//    //    计算每一行的字数，往下取值，
+//    NSInteger wordsPerLine = floor(width /fontSize);
+//    
+//    //    得到取值后的每一行的宽度
+//    CGFloat widthPerLine = fontSize *wordsPerLine;
+//    
+//    //    得到文本的总行数
+//    NSInteger nLines = ceil(size.width/widthPerLine );
+//    //    根据行数得到文本的高度
+//    CGFloat heigh = nLines *(size.height);
+//    
+//    return heigh;
+//    
+//    
+//}
+
+@end
